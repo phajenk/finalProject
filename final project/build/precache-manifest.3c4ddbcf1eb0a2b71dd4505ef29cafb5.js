3c8da43ec2b7dd453cac0dcd8ebfe179e26b18e2
phajenk <felix.fxh@gmail.com>
2022-08-29 18:24:11 +0700

    Add files via upload

---
+++ final project/build/precache-manifest.3c4ddbcf1eb0a2b71dd4505ef29cafb5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f77b6058d94b4950d9a",
+    "url": "/static/css/main.6069665e.chunk.css"
+  },
+  {
+    "revision": "8f77b6058d94b4950d9a",
+    "url": "/static/js/main.8f77b605.chunk.js"
+  },
+  {
+    "revision": "01e80e744d77571ab563",
+    "url": "/static/js/1.01e80e74.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "537c04980b28b6c09b35c8744ff5ec6d",
+    "url": "/index.html"
+  }
+];
---
+++ final project/src/components/Movie.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const DEFAULT_PLACEHOLDER_IMAGE =
+  "https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg";
+
+const Movie = ({ movie }) => {
+  const poster =
+    movie.Poster === "N/A" ? DEFAULT_PLACEHOLDER_IMAGE : movie.Poster;
+  const linkIMDB = 'https://www.imdb.com/title/'+movie.imdbID;
+  return (
+    <div className="movie">
+      <a href={linkIMDB} target="_blank">
+      <h2>{movie.Title}</h2>
+      <div>
+        
+        <img
+          width="200"
+          alt={`The movie titled: ${movie.Title}`}
+          src={poster}
+        />
+      </div>
+      <p>({movie.Year})</p>
+      
+      </a>
+    </div>
+  );
+};
+
+export default Movie;
